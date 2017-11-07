6963a78243effd0444ff656884077f0dcf5e1247
Michael Sharman <michael@thecodesharman.com.au>
2017-11-07 23:41:04 +1100

    added setter for Person.name

--- bbf-bioblitz-client/app/models/person.js
+++ bbf-bioblitz-client/app/models/person.js
@@ -1,8 +1,20 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   memberOf: DS.hasMany('team'),
   capabilities: DS.hasMany('role'),
+  name: Ember.computed('firstName', 'lastName', {
+    get(key) {
+      return `${this.get('firstName')} ${this.get('lastName')}`;
+    },
+    set(key, value) {
+      var [firstName, lastName] = value.split(/\s+/);
+      this.set('firstName', firstName);
+      this.set('lastName',  lastName);
+      return value;
+    }
+  })
 });
