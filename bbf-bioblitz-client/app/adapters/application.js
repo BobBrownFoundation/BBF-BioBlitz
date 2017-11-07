dbcb6a52b77bc4c11d1f2035ee8d49a9814e80f9
Michael Sharman <michael@thecodesharman.com.au>
2017-11-07 23:42:08 +1100

    random updates

--- bbf-bioblitz-client/app/adapters/application.js
+++ bbf-bioblitz-client/app/adapters/application.js
@@ -4,11 +4,12 @@ import ENV from 'bbf-bioblitz/config/environment';
 export default DS.JSONAPIAdapter.extend({
   host: ENV.APP.host,
   dataForRequest(params) {
-
+      let attrs = null;
       // Encode commas to %%2C in order to get them past the Lux filter code
       switch( params.requestType ) {
+
         case 'query':
-          let attrs = params.query.filter;
+          attrs = params.query.filter;
           Object.keys(attrs)
             .forEach( (key) => attrs[key] = attrs[key].replace(',','%2C') );
           break;
--- bbf-bioblitz-client/tests/test-helper.js
+++ bbf-bioblitz-client/tests/test-helper.js
@@ -2,3 +2,9 @@ import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
 
 setResolver(resolver);
+
+if (window.location.search.indexOf('nocontainer') > -1) {
+    Ember.$('#ember-testing-container').css({
+        visibility: 'hidden'
+    });
+}
