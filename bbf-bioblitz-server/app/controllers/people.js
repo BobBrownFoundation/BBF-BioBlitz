35f4ce6e7f5265482f304802498710f0420d2421
Michael Sharman <michael@thecodesharman.com.au>
2017-11-07 23:39:22 +1100

    added filter to enable searching via name

--- bbf-bioblitz-server/app/controllers/people.js
+++ bbf-bioblitz-server/app/controllers/people.js
@@ -1,10 +1,29 @@
 import { Controller } from 'lux-framework';
+import Person from 'app/models/person';
 
 class PeopleController extends Controller {
-  params = [
+  filter = [
       'lastName',
-      'firstName'
+      'firstName',
+      'name'
     ];
+
+  beforeAction = [
+    // If a filter is supplied with the filter parameter "name" then this is
+    // transparently converted into a filter with firstName and lastName set
+    // appropriately.
+    function filterByName(request) {
+      let filter = request.params.filter;
+      if ( filter.name ) {
+        let name = filter.name;
+        delete filter.name;
+        let [ firstName, lastName ] = name.split(/\s+/);
+        filter.firstName = firstName;
+        filter.lastName = lastName;
+      }
+    }
+  ];
+
 }
 
 export default PeopleController;
