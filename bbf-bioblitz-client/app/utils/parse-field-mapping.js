d55bc6a43df62c47682498e7519f9d04140a8229
Michael Sharman <michael@thecodesharman.com.au>
2017-11-11 03:29:22 +1100

    implemented composite key lookup

--- bbf-bioblitz-client/app/utils/parse-field-mapping.js
+++ bbf-bioblitz-client/app/utils/parse-field-mapping.js
@@ -1,57 +1,63 @@
 import Ember from 'ember';
 
-export default function parseFieldMapping( root, mapping, store ) {
-  function parseColumnName( fieldName ) {
-    let parts = fieldName.split(':');
-    return parts.map( n => n.camelize() );
-  }
+function parseColumnName( fieldName ) {
+  let parts = fieldName.split(':');
+  return parts.map( n => n.camelize() );
+}
 
-  /*
-    Pass an array of field names and model e.g.
-      rootModel = Surveyslot, fieldChain = [ 'participants', 'person', 'name' ]
-    returns an array that looks like this:
-      [
-        { model: Person, kind: 'value', field: 'name'},
-        { model: Participant, kind: 'hasOne', field: 'person' },
-        { model: Surveyslot, kind:'hasMany', field: 'participants' }
-      ]
-  */
-  function expandModelChain( rootModel, fieldChain ) {
+/*
+  Pass an array of field names and model e.g.
+    rootModel = Surveyslot, fieldChain = [ 'participants', 'person', 'name' ]
+  returns an array that looks like this:
+    [
+      { model: Person, kind: 'value', field: 'name'},
+      { model: Participant, kind: 'hasOne', field: 'person' },
+      { model: Surveyslot, kind:'hasMany', field: 'participants' }
+    ]
+*/
+function expandModelChain( rootModel, fieldChain, store ) {
+  // default to name on the rootModel
+  if ( fieldChain.length === 0 )
+      return [{ model: rootModel, kind: 'value', field: 'name'}];
 
-    if ( !rootModel ) {
-      let model = store.modelFor( fieldChain[0] );
-      let fieldName;
-      if ( fieldChain.length === 2 )
-        fieldName = fieldChain[1];
-      else
-        fieldName = 'name'; // default
-      return [{ model: model, kind: 'value', field: fieldName }];
-    }
-
-    // base case
-    if ( fieldChain.length === 1 )
-        return [{ model: rootModel, kind: 'value', field: fieldChain[0]}];
-
-    // construct n based on n-1
-    let [ indirectField, ... restOfChain ] = fieldChain;
-    let relationshipMeta =
-      Ember.get( rootModel, 'relationshipsByName')
-        .get( indirectField );
-
-    if ( typeof relationshipMeta === 'undefined')
-      throw new Error(`Unable to find relationship ${indirectField} in ${rootModel.modelName}`);
-
-    let refModel =  store.modelFor( relationshipMeta.type );
-
-    if ( typeof refModel === 'undefined')
-      throw new Error(`Unable to find type for relationship ${indirectField} in ${rootModel.modelName}`);
-
-    let expansion = expandModelChain( refModel, restOfChain );
-    expansion.push( {model: rootModel, kind: relationshipMeta.kind,
-      field: indirectField });
-
+  // If no root model then treat the first part of the chain as the root
+  // model name and recurse. If the root model is specified but the
+  // first part as interpreted as a model matches the root then skip.
+  let modelName = fieldChain[0].dasherize();
+  if ( !rootModel || modelName === rootModel.modelName ) {
+    fieldChain.shift();
+    let expansion = expandModelChain( store.modelFor(modelName), fieldChain, store );
+    expansion.slice(-1)[0].useAsKey = true;
     return expansion;
-
   }
-  return expandModelChain( root, parseColumnName( mapping ) );
+
+  // base case
+  if ( fieldChain.length === 1 )
+      return [{ model: rootModel, kind: 'value', field: fieldChain[0]}];
+
+  // construct n based on n-1
+  let [ indirectField, ... restOfChain ] = fieldChain;
+  let relationshipMeta =
+    Ember.get( rootModel, 'relationshipsByName')
+      .get( indirectField );
+
+  if ( typeof relationshipMeta === 'undefined')
+    throw new Error(`Unable to find relationship ${indirectField} in ${rootModel.modelName}`);
+
+  let refModel =  store.modelFor( relationshipMeta.type );
+
+  if ( typeof refModel === 'undefined')
+    throw new Error(`Unable to find type for relationship ${indirectField} in ${rootModel.modelName}`);
+
+  let expansion = expandModelChain( refModel, restOfChain, store );
+  expansion.push( {model: rootModel, kind: relationshipMeta.kind,
+    field: indirectField });
+
+  return expansion;
+
+}
+
+export default function parseFieldMapping( root, mapping, store ) {
+
+  return expandModelChain( root, parseColumnName( mapping ), store );
 }
---
+++ bbf-bioblitz-client/tests/unit/utils/parse-field-mapping-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import parseFieldMapping from 'bbf-bioblitz/utils/parse-field-mapping';
+
+describe('Unit | Utility | parse field mapping', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    let result = parseFieldMapping();
+    expect(result).to.be.ok;
+  });
+});
